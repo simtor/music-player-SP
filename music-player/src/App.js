4940dcfaccc469bb74a2a1ad9837a08a5ed6fbbc
simtor <simone.ram@ada.ac.uk>
2021-10-19 15:18:02 +0100

    Added data and buttons

--- music-player/src/App.js
+++ music-player/src/App.js
@@ -1,24 +1,60 @@
-// import logo from './logo.svg';
-// import './App.css';
+import {useState, useEffect} from 'react';
+import Player from './components/Player/Player';
+
 
 function App() {
+  const [songs] = useState([
+    {
+      title: "Life Of The Party (feat. Andre 3000)",
+      artist: "Kanye West",
+      img_src: "./images/song-1.jpg",
+      src: "./music/Kanye West - Life Of The Party (feat. Andre 3000).mp3"
+    },
+    {
+      title: "Flashing Lights",
+      artist: "Kanye West",
+      img_src: "./images/song-2.png",
+      src: "./music/Flashing Lights.mp3"
+    },
+    {
+      title: "I'm God",
+      artist: "Clams Casino",
+      img_src: "./images/song-3.jpg",
+      src: "./music/Clams Casino - I'm God.mp3"
+    },
+    {
+      title: "Just For Now",
+      artist: "Imogen Heap - Just For Now",
+      img_src: "./images/song-4.jpg",
+      src: "./music/Imogen Heap - Just For Now.mp3"
+    }
+  ]);
 
-  function playMusic(){
-    console.log("Music play")
-  }
-  function StopMusic(){
-    console.log("Music stop")
-  }
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(0);
+
+
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex]);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <button onClick={playMusic}> Play</button>
-        <button onClick={StopMusic}> Stop</button>
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-      </header>
+      <Player 
+        currentSongIndex={currentSongIndex} 
+        setCurrentSongIndex={setCurrentSongIndex} 
+        nextSongIndex={nextSongIndex} 
+        songs={songs}
+      />
     </div>
   );
 }
 
 export default App;
+
--- music-player/src/trash.js
+++ music-player/src/trash.js
@@ -64,3 +64,28 @@
 // }
 
 // export default App;
+//////////////////////////////////
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+
+//   function playMusic(){
+//     console.log("Music play")
+//   }
+//   function StopMusic(){
+//     console.log("Music stop")
+//   }
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <button onClick={playMusic}> Play</button>
+//         <button onClick={StopMusic}> Stop</button>
+//         {/* <img src={logo} className="App-logo" alt="logo" /> */}
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
